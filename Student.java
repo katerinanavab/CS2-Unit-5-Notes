203465e008f67ccc3d4f404c8d90db0a4fc014ce
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-12-06 14:40:49 +0000

    declared the Student class

---
+++ Student.java
@@ -0,0 +1,17 @@
+/* Example CLASS that defines an OBJECT
+Class names like Student are always capitalized!
+Object-Oriented Classes are like BLUEPRINTS
+(in another Java class, you create instances from this blueprint)
+*/
+public class Student {
+    // 1. INSTANCE VARIABLES (attributes/data)
+    // think of what this object "needs to know"
+    String name; // "a Student has a name"
+    int gradYear; 
+
+    // 2. CONSTRUCTORS (manuals for setting up an object)
+
+
+    // 3. METHODS (behaviors associated w/the object type)
+
+}
